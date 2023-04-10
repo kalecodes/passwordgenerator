c3121a047caa5d242d9c13282dc9e8dfefb497df
kalen510 <kalen.wiley@gmail.com>
2023-04-10 14:24:18 -0400

    password length prompt

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // Assignment code here
+var generatePassword = function() {
+  // prompt user for password length
+  var passwordLength = window.prompt("How many characters long should the password be? (Please choose an option between 8 and 128 characters)");
+  // convert response string to integer
+  passwordLength = parseInt(passwordLength);
+  // validate response
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please enter a number between 8 and 128");
+    generatePassword();
+  }
 
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
